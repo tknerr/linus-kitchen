cb5e7fbb33f65dd3f92e2825f77acbcab6de9288
Torben <tkn@zuehlke.com>
2013-09-28 06:08:46 +0200

    add ruby-dev package which is required to install some gems like nokogire (require mkmf)

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -1,6 +1,6 @@
 
 # XXX: consider using chef-rvm or rubybuild cookbook
-%w{ ruby1.9.1 build-essential libxslt-dev libxml2-dev }.each do |pkg|
+%w{ ruby1.9.1 ruby1.9.1-dev build-essential libxslt-dev libxml2-dev }.each do |pkg|
   package pkg
 end
 
