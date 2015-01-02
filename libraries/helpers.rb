699ec04368efc8890029375638d6aa9418ec52bb
Torben Knerr <mail@tknerr.de>
2015-01-02 02:27:39 +0100

    also make the installation of vagrant plugins properly idempotent so it is reported as up-to-date within the chef doc formatter

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -5,15 +5,15 @@ class Chef
     # supports passing the VAGRANT_HOME environment
     #
     def install_vagrant_plugin(name, version)
+      env = {
+        HOME => "/home/#{node['devbox']['user']}"
+      }
       bash "install vagrant plugin #{name}-#{version} for #{node['devbox']['user']}" do
         user node['devbox']['user']
         group node['devbox']['group']
-        environment "HOME" => "/home/#{node['devbox']['user']}"
-        code <<-EOH
-        if ! $(vagrant plugin list | grep -q '#{name} (#{version})'); then
-          vagrant plugin install #{name} --plugin-version #{version}
-        fi
-        EOH
+        environment env
+        code "vagrant plugin install #{name} --plugin-version #{version}"
+        not_if "vagrant plugin list | grep -q '#{name} (#{version})'", :environment => env
       end
     end
   end
