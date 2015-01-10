b9b813c94e22fb34cc91919d297614eb1969d548
Torben Knerr <mail@tknerr.de>
2015-01-10 13:31:54 +0100

    install lxc first so that whitelisting lxc commands via `vagrant lxc sudoers` works by the first time

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,6 @@ include_recipe "dev-box::user"
 include_recipe "dev-box::bash"
 include_recipe "dev-box::chefdk"
 #include_recipe "dev-box::ruby"
-include_recipe "dev-box::vagrant"
 include_recipe "dev-box::lxc"
+include_recipe "dev-box::vagrant"
 
