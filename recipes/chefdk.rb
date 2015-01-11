2ead112f7fbc4e4976273a8628b78afd6c7100ca
Torben Knerr <mail@tknerr.de>
2015-01-11 14:10:00 +0100

    explicitly set `BUNDLE_PATH` via global bundler config to fix the bundler issue from test-kitchen/test-kitchen#575

--- recipes/chefdk.rb
+++ recipes/chefdk.rb
@@ -6,3 +6,18 @@ bash_profile "chefdk-shell-init" do
   user devbox_user
   content %{eval "$(chef shell-init bash)"}
 end
+
+
+directory "#{devbox_userhome}/.bundle" do
+  owner devbox_user
+  group devbox_group
+  mode "0755"
+  action :create
+end
+
+template "#{devbox_userhome}/.bundle/config" do
+  source "bundler_config.erb"
+  owner devbox_user
+  group devbox_group
+  mode "0644"
+end
