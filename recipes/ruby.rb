e75eca23bc119de29dd530ce92da3697e9a42ef5
Torben Knerr <mail@tknerr.de>
2015-02-04 07:40:00 +0100

    remove the ruby recipe because chefdk is our default ruby!

--- recipes/ruby.rb
+++
@@ -1,7 +0,0 @@
-
-# XXX: consider using chef-rvm or rubybuild cookbook
-%w{ ruby1.9.1 ruby1.9.1-dev build-essential libxslt-dev libxml2-dev }.each do |pkg|
-  package pkg
-end
-
-gem_package "bundler"
