d1cbb157f95e8644b85b0524ceced9b625d3c32c
Torben Knerr <mail@tknerr.de>
2015-09-08 21:26:52 +0200

    setup alias 'be' for 'bundle exec'

--- recipes/bash.rb
+++ recipes/bash.rb
@@ -1,5 +1,10 @@
 
 #
-# setup .bash.d for the devbox user 
+# setup .bash.d for the devbox user
 #
 bashd devbox_user
+
+bashd_entry "shell-aliases" do
+  user devbox_user
+  content 'alias be="bundle exec"'
+end
