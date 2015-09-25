15097a9e9fa54de6a0da475cfd619711789c2504
Torben Knerr <mail@tknerr.de>
2015-09-25 18:51:20 +0200

    remove everything under cookbooks/vm

--- cookbooks/vm/metadata.rb
+++ cookbooks/vm/metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Torben Knerr'
 maintainer_email 'mail@tknerr.de'
 license          'All rights reserved'
 description      'Installs/Configures dev-box'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+long_description IO.read(File.join(File.dirname(__FILE__), '../../README.md'))
 version          '0.1.0'
 
 supports "ubuntu"
