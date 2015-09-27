a748fa614e3e8450d5e2bbe9b253eaabaccfea61
Torben Knerr <mail@tknerr.de>
2015-09-28 01:19:00 +0200

    add separate specs for the `update-vm.sh` script

--- cookbooks/vm/spec/integration/recipes/base_spec.rb
+++ cookbooks/vm/spec/integration/recipes/base_spec.rb
@@ -4,11 +4,6 @@ describe 'vm::base' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
 
-  it 'installs git' do
-    expect(package('git')).to be_installed
-    expect(command('git --version').exit_status).to eq 0
-  end
-
   it 'installs vim' do
     expect(package('vim')).to be_installed
     expect(command('vim --version').exit_status).to eq 0
---
+++ cookbooks/vm/spec/integration/scripts/update_vm_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe 'update-vm.sh' do
+  # Serverspec examples can be found at
+  # http://serverspec.org/resource_types.html
+
+  it 'installs git' do
+    expect(package('git')).to be_installed
+    expect(command('git --version').exit_status).to eq 0
+  end
+
+  it 'installs chefdk 0.7.0' do
+    expect(command('chef --version').stdout).to contain 'Chef Development Kit Version: 0.7.0'
+  end
+end
