d8def26e3571c8b0688e506d4c2cd65bcf7aa6c1
Torben Knerr <mail@tknerr.de>
2015-11-05 03:07:01 +0100

    as we skip the installation of atom in docker we should also skip the tests

--- cookbooks/vm/spec/integration/recipes/atom_spec.rb
+++ cookbooks/vm/spec/integration/recipes/atom_spec.rb
@@ -1,31 +1,36 @@
 require 'spec_helper'
+require 'chef/sugar/docker'
 
-describe 'vm::atom' do
+# installing atom on docker does not work, so we don't need to test either
+unless Chef::Sugar::Docker.docker?(@node)
 
-  let(:atom_version) { devbox_user_command('atom -v').stdout.strip }
-  let(:atom_config) { file('/home/vagrant/.atom/config.cson') }
-  let(:installed_plugins) { devbox_user_command('apm list -i').stdout }
+  describe 'vm::atom' do
 
-  it 'installs atom 1.1.0' do
-    expect(atom_version).to eq '1.1.0'
-  end
+    let(:atom_version) { devbox_user_command('atom -v').stdout.strip }
+    let(:atom_config) { file('/home/vagrant/.atom/config.cson') }
+    let(:installed_plugins) { devbox_user_command('apm list -i').stdout }
 
-  it 'installs some useful atom plugins' do
-    expect(installed_plugins).to contain 'atom-beautify'
-    expect(installed_plugins).to contain 'minimap'
-    expect(installed_plugins).to contain 'line-ending-converter'
-    expect(installed_plugins).to contain 'language-chef'
-    expect(installed_plugins).to contain 'language-batchfile'
-  end
+    it 'installs atom 1.1.0' do
+      expect(atom_version).to eq '1.1.0'
+    end
 
-  it 'configures atom to have sublime tabs behaviour' do
-    expect(atom_config).to contain 'usePreviewTabs: true'
-  end
-  it 'configures atom to show invisible characters' do
-    expect(atom_config).to contain 'showInvisibles: true'
-  end
-  it 'configures atom to use the "atom-dark" theme' do
-    expect(atom_config).to contain '"atom-dark-ui"'
-    expect(atom_config).to contain '"atom-dark-syntax"'
+    it 'installs some useful atom plugins' do
+      expect(installed_plugins).to contain 'atom-beautify'
+      expect(installed_plugins).to contain 'minimap'
+      expect(installed_plugins).to contain 'line-ending-converter'
+      expect(installed_plugins).to contain 'language-chef'
+      expect(installed_plugins).to contain 'language-batchfile'
+    end
+
+    it 'configures atom to have sublime tabs behaviour' do
+      expect(atom_config).to contain 'usePreviewTabs: true'
+    end
+    it 'configures atom to show invisible characters' do
+      expect(atom_config).to contain 'showInvisibles: true'
+    end
+    it 'configures atom to use the "atom-dark" theme' do
+      expect(atom_config).to contain '"atom-dark-ui"'
+      expect(atom_config).to contain '"atom-dark-syntax"'
+    end
   end
 end
