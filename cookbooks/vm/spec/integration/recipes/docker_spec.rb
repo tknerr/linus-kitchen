91c32f5daafbec5f9933f48e75ff62cab386ca54
Torben Knerr <mail@tknerr.de>
2015-11-05 15:09:39 +0100

    add integration tests for docker recipe

---
+++ cookbooks/vm/spec/integration/recipes/docker_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe 'vm::docker' do
+
+  it 'installs docker 1.8.1' do
+    expect(devbox_user_command('docker -v').stdout).to match 'Docker version 1.8.1'
+  end
+
+  it 'adds the devbox user to the docker group' do
+    expect(devbox_user_command('id').stdout).to match(/groups=.*\(docker\)/)
+  end
+
+  it 'allows to run docker commands without sudo' do
+    expect(devbox_user_command('docker ps').stdout).to contain 'CONTAINER ID'
+  end
+end
