77f166fead0aa13e7e0f05f257695f314eb014ad
Torben Knerr <mail@tknerr.de>
2015-12-15 17:49:02 +0100

    forgot to include the git recipe by default

--- cookbooks/vm/recipes/default.rb
+++ cookbooks/vm/recipes/default.rb
@@ -2,6 +2,7 @@
 include_recipe 'vm::base'
 include_recipe 'vm::user'
 include_recipe 'vm::bash'
+include_recipe 'vm::git'
 include_recipe 'vm::chefdk'
 include_recipe 'vm::vagrant'
 include_recipe 'vm::atom'
