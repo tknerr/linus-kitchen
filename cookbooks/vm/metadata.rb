8cc8a035c4179ceded3adea0533f17dcf7635781
Torben Knerr <mail@tknerr.de>
2016-05-10 17:54:39 +0200

    fix foodcritic complaints (FC064 and FC065)

--- cookbooks/vm/metadata.rb
+++ cookbooks/vm/metadata.rb
@@ -5,6 +5,8 @@ license 'All rights reserved'
 description 'Installs/Configures the Linus Kitchen VM'
 long_description IO.read(File.join(File.dirname(__FILE__), '../../README.md'))
 version '0.1.0'
+issues_url 'https://github.com/tknerr/linus-kitchen/issues'
+source_url 'https://github.com/tknerr/linus-kitchen'
 
 supports 'ubuntu'
 
