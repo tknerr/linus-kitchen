536efb01d6f78e40702adf012d29b28d86721d5f
Torben Knerr <mail@tknerr.de>
2017-04-08 01:06:12 +0200

    ensure the docker group exists before starting the service

--- cookbooks/vm/recipes/docker.rb
+++ cookbooks/vm/recipes/docker.rb
@@ -2,6 +2,13 @@
 # add the docker apt repository
 include_recipe 'chef-apt-docker'
 
+# create the docker group and add the devbox_user to it
+group 'docker' do
+  action :create
+  members devbox_user
+  append true
+end
+
 # install docker and start the docker deamon
 docker_service 'default' do
   install_method 'package'
@@ -9,12 +16,4 @@ docker_service 'default' do
   package_options "--force-yes -o Dpkg::Options::='--force-confold' -o Dpkg::Options::='--force-all'"
   service_manager docker? ? 'execute' : 'systemd'
   action [:create, :start]
-end
-
-# create the docker group and add the devbox_user to it
-group 'docker' do
-  action :create
-  members devbox_user
-  append true
-  notifies :restart, 'docker_service[default]', :immediately
 end
