ef2f465406bfed9e29afbd6baf60b3515c5ab101
Torben Knerr <mail@tknerr.de>
2017-04-08 03:32:43 +0200

    fix file mode specification

--- cookbooks/vm/recipes/virtualbox.rb
+++ cookbooks/vm/recipes/virtualbox.rb
@@ -5,7 +5,7 @@ vbox_deb_file = 'virtualbox-5.1_5.1.18-114002~Ubuntu~xenial_amd64.deb'
 # download virtualbox
 remote_file "#{Chef::Config[:file_cache_path]}/#{vbox_deb_file}" do
   source "http://download.virtualbox.org/virtualbox/#{vbox_version}/#{vbox_deb_file}"
-  mode 0644
+  mode '0644'
 end
 
 dpkg_package vbox_deb_file do
