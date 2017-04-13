6d16892fc9c3659d501e5d4a6db74c267837065e
Torben Knerr <mail@tknerr.de>
2017-04-13 08:03:59 +0200

    force colored output for rspec even if not running via tty (via undocumented --tty option)

--- scripts/update-vm.sh
+++ scripts/update-vm.sh
@@ -104,7 +104,7 @@ verify_vm() {
 
   # run integration tests
   step "run integration tests"
-  rspec --require rspec_junit_formatter --format doc --color --format RspecJunitFormatter --out test/junit-report.xml --format html --out test/test-report.html
+  rspec --require rspec_junit_formatter --format doc --color --tty --format RspecJunitFormatter --out test/junit-report.xml --format html --out test/test-report.html
 }
 
 #
