4810fdd5bba071a2331e865589c165d9879ec54e
Torben Knerr <mail@tknerr.de>
2017-04-13 09:10:11 +0200

    align atom recipe with vagrant recipe and put plugin definitions on top of the file

--- cookbooks/vm/recipes/atom.rb
+++ cookbooks/vm/recipes/atom.rb
@@ -2,6 +2,12 @@
 atom_version = '1.15.0'
 atom_deb_file = "atom-v#{atom_version}-amd64.deb"
 
+atom_plugins = {
+  'atom-beautify': '0.29.18',
+  'minimap': '4.27.1',
+  'language-chef': '0.9.0'
+}
+
 if docker?
   # we need libxss-dev for starting atom in docker
   package 'libxss-dev'
@@ -9,7 +15,7 @@ if docker?
   extra_options = '--no-install-recommends'
 end
 
-# install atom
+# install atom editor
 remote_file "#{Chef::Config[:file_cache_path]}/#{atom_deb_file}" do
   source "https://github.com/atom/atom/releases/download/v#{atom_version}/atom-amd64.deb"
   mode '0644'
@@ -22,13 +28,8 @@ bash 'install-atom-with-dependencies' do
   not_if "which atom && xvfb-run atom -v | grep -q '#{atom_version}'"
 end
 
-# install plugins
-plugins = {
-  'atom-beautify' => '0.29.18',
-  'minimap' => '4.27.1',
-  'language-chef' => '0.9.0'
-}
-plugins.each do |name, version|
+# install atom plugins
+atom_plugins.each do |name, version|
   install_atom_plugin(name, version)
 end
 
