4ac84fc9e693fda819497b6bb29854a56edbab02
Torben Knerr <mail@tknerr.de>
2017-04-13 17:20:19 +0200

    add setup-user.sh script for creating a new user in the VM

---
+++ scripts/setup-user.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+LOGIN_USER=$1
+LOGIN_PASS=$2
+
+# create the login user if it does not already exist
+if getent passwd $LOGIN_USER >/dev/null; then
+  echo "Login User '$LOGIN_USER' already created..."
+else
+  # create user, set password and add it to the usual groups
+  adduser $LOGIN_USER --gecos "" --home "/home/$LOGIN_USER" --disabled-password
+  echo "$LOGIN_USER:$LOGIN_PASS" | chpasswd
+  usermod -a -G adm,cdrom,sudo,dip,plugdev,lpadmin,sambashare $LOGIN_USER
+
+  # ensure the new user can do passwordless sudo
+  echo "$LOGIN_USER ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/$LOGIN_USER
+fi
