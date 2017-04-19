4727f9ad38983f5e7d99417b36653e87ac2b5c05
Torben Knerr <mail@tknerr.de>
2017-04-20 00:54:29 +0200

    check for file existence instead of using wget's --no-clobber option

--- scripts/update-vm.sh
+++ scripts/update-vm.sh
@@ -34,9 +34,12 @@ check_chefdk() {
   else
     step "Downloading and installing ChefDK $CHEFDK_VERSION"
     mkdir -p $TARGET_DIR
-    wget --no-verbose --no-clobber -O $TARGET_DIR/chefdk_$CHEFDK_VERSION-1_amd64.deb \
-      https://packages.chef.io/files/current/chefdk/$CHEFDK_VERSION/ubuntu/16.04/chefdk_$CHEFDK_VERSION-1_amd64.deb
-    sudo dpkg -i $TARGET_DIR/chefdk_$CHEFDK_VERSION-1_amd64.deb
+    local CHEFDK_DEB=chefdk_$CHEFDK_VERSION-1_amd64.deb
+    local CHEFDK_URL=https://packages.chef.io/files/current/chefdk/$CHEFDK_VERSION/ubuntu/16.04/$CHEFDK_DEB
+    if [ ! -f $TARGET_DIR/$CHEFDK_DEB ]; then
+      wget -O $TARGET_DIR/$CHEFDK_DEB $CHEFDK_URL
+    fi
+    sudo dpkg -i $TARGET_DIR/$CHEFDK_DEB
   fi
 }
 
