29b308116ae41ac798f89c39a7f9cd7a35a0f5d1
Torben Knerr <mail@tknerr.de>
2017-05-25 01:57:42 +0200

    add checksum for virtualbox download

--- cookbooks/vm/recipes/virtualbox.rb
+++ cookbooks/vm/recipes/virtualbox.rb
@@ -1,10 +1,12 @@
 
 vbox_version = '5.1.22'
 vbox_deb_file = 'virtualbox-5.1_5.1.22-115126~Ubuntu~xenial_amd64.deb'
+vbox_checksum = '4d964bb498284906c359de049c828199f97b6bfb1867005647b61bdffa6bdaf7'
 
 # download virtualbox
 remote_file "#{Chef::Config[:file_cache_path]}/#{vbox_deb_file}" do
   source "http://download.virtualbox.org/virtualbox/#{vbox_version}/#{vbox_deb_file}"
+  checksum vbox_checksum
   mode '0644'
 end
 
