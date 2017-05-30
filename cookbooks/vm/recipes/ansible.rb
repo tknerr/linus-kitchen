400895a98be56c2d5c5ffb853f52ac8adca0c1d6
Torben Knerr <mail@tknerr.de>
2017-05-30 08:36:40 +0200

    install ansible-lint 3.4.12

--- cookbooks/vm/recipes/ansible.rb
+++ cookbooks/vm/recipes/ansible.rb
@@ -6,6 +6,7 @@ package ['python-pip', 'libssl-dev', 'libffi-dev']
 install_pip_package 'ansible', '2.3.0.0'
 
 # install testinfra with the spec formatter, and molecule
+install_pip_package 'ansible-lint', '3.4.12'
 install_pip_package 'testinfra', '1.6.2'
 install_pip_package 'pytest-spec', '1.1.0'
 install_pip_package 'molecule', '2.0.0.0rc5'
--- cookbooks/vm/spec/integration/recipes/ansible_spec.rb
+++ cookbooks/vm/spec/integration/recipes/ansible_spec.rb
@@ -3,11 +3,15 @@ require 'spec_helper'
 describe 'vm::ansible' do
 
   let(:ansible_version) { vm_user_gui_command('ansible --version').stdout }
+  let(:ansible_lint_version) { vm_user_gui_command('ansible-lint --version').stdout }
   let(:pytest_version) { vm_user_gui_command('pytest --version').stdout }
   let(:molecule_version) { vm_user_gui_command('molecule --version').stdout }
 
   it 'installs ansible 2.3.0.0' do
     expect(ansible_version).to contain '2.3.0.0'
+  end
+  it 'installs ansible-lint 3.4.12' do
+    expect(ansible_lint_version).to contain '3.4.12'
   end
   it 'installs testinfra 1.6.2' do
     expect(pytest_version).to contain 'testinfra-1.6.2'
