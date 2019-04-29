dc4e243dd2bc1b01b0368dd1e52bad201f56b902
Torben Knerr <mail@tknerr.de>
2019-04-29 13:46:42 +0200

    update to latest Packer 1.4.0

--- cookbooks/vm/spec/integration/recipes/packer_spec.rb
+++ cookbooks/vm/spec/integration/recipes/packer_spec.rb
@@ -4,7 +4,7 @@ describe 'vm::packer' do
 
   let(:packer_version) { vm_user_command('packer -v').stdout.strip }
 
-  it 'installs packer 1.3.2' do
-    expect(packer_version).to eq '1.3.2'
+  it 'installs packer 1.4.0' do
+    expect(packer_version).to eq '1.4.0'
   end
 end
