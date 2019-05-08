76d184c704040ec881d280859ca28adeb36f0011
Torben Knerr <mail@tknerr.de>
2019-05-08 19:00:51 +0200

    add comments

--- cookbooks/vm/recipes/docker.rb
+++ cookbooks/vm/recipes/docker.rb
@@ -8,18 +8,19 @@ group 'docker' do
   append true
 end
 
+# install docker CLI and deamon packages
 docker_installation_package 'default' do
   package_name 'docker-ce-cli'
   version docker_version
   action :create
 end
-
 docker_installation_package 'default' do
   package_name 'docker-ce'
   version docker_version
   action :create
 end
 
+# install docker daemon as a service
 docker_service_manager_systemd 'default' do
   action :start
 end
