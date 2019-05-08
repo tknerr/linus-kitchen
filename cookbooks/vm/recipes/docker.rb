697af10c25e273ad1912095d77969704bed5510e
Torben Knerr <mail@tknerr.de>
2019-05-08 09:46:46 +0200

    next try

--- cookbooks/vm/recipes/docker.rb
+++ cookbooks/vm/recipes/docker.rb
@@ -12,11 +12,9 @@ docker_installation_package 'default' do
   package_name 'docker-ce-cli'
   version docker_version
 end
-docker_installation_package 'default' do
-  package_name 'docker-ce'
-  version docker_version
-end
-docker_service_manager 'default' do
-  action :start
+docker_service 'default' do	
+  install_method 'package'	
+  version docker_version	
+  action [:create, :start]
 end
 
