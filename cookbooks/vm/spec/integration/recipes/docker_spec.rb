1ed7f1eac8bd74d8902ea55f82dbe9665ae7b3eb
Torben Knerr <mail@tknerr.de>
2019-05-08 11:28:52 +0200

    check for both Docker client and server version

--- cookbooks/vm/spec/integration/recipes/docker_spec.rb
+++ cookbooks/vm/spec/integration/recipes/docker_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe 'vm::docker' do
 
   it 'installs docker 18.09.5' do
-    expect(vm_user_command('docker -v').stdout).to match 'Docker version 18.09.5'
+    expect(vm_user_command("docker version --format '{{.Client.Version}}'").stdout).to match '18.09.5'
+    expect(vm_user_command("docker version --format '{{.Server.Version}}'").stdout).to match '18.09.5'
   end
 
   it 'adds the vm user to the docker group' do
